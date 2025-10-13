c0db6b8e5f40911a07c5c34c9c70f6126890528a
Mary 254 <maryndirangu96@gmail.com>
2025-10-13 05:41:25 +0300

    add html,css and js files and their for form validation

---
+++ script.js
@@ -0,0 +1,55 @@
+// wrap the the entire script in a DOMContentLoaded event listener to ensure js runs after html loads
+document.addEventListener("DOMContentLoaded", function(){
+ // Select the form and feedback division
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById("form-feedback");
+
+//listen for submission
+form.addEventListener("submit", function(event){
+    event.preventDefault(); 
+
+    //retrieve and trim white spaces in input values 
+    const userName = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    // validating
+    let isValid = true;
+    let messages = []
+
+    //username vallidation
+    if (userName.length < 3){
+        isValid = false;
+        messages.push("Username must be at least 3 characters long.")
+    }
+
+    //email vallidation
+    if (!email.includes("@") || !email.includes(".")) {
+      isValid = false;
+      messages.push("Please enter a valid email address.");
+    }
+
+    // Password validation
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password must be at least 8 characters long.");
+    }
+
+
+    //Display feedback
+    feedbackDiv.style.display = "block";
+
+    if(isValid){
+        feedbackDiv.textContent = "Registration successful!";
+        feedbackDiv.style.color = "#28a745"; // green success color
+        feedbackDiv.style.backgroundColor = "#d4edda";
+    } else{
+        feedbackDiv.innerHTML = messages.join("<br>");
+        feedbackDiv.style.color = "#dc3545"; // red error color
+        feedbackDiv.style.backgroundColor = "#f8d7da";
+    }
+
+
+});
+
+});
